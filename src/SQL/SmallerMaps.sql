3636891aae3b3a222d7494062e0404c0688a4ace
bmaupin <bmaupin@users.noreply.github.com>
2026-05-01 14:32:21 -0400

    feat: Increase map size
    0.4 was too small for a continents game; one of the players couldn't build a city

--- src/SQL/SmallerMaps.sql
+++ src/SQL/SmallerMaps.sql
@@ -4,14 +4,14 @@ SET
     GridWidth = CASE
         -- Calculate new size, round to nearest INT, then subtract 1 if odd to make sure it's even
         -- Use MAX to set minimum map size; smaller sizes might cause crashes
-        WHEN ROUND(GridWidth * 0.4) % 2 = 1
-            THEN MAX(16, ROUND(GridWidth * 0.4) - 1)
-        ELSE MAX(16, ROUND(GridWidth * 0.4))
+        WHEN ROUND(GridWidth * 0.45) % 2 = 1
+            THEN MAX(16, ROUND(GridWidth * 0.45) - 1)
+        ELSE MAX(16, ROUND(GridWidth * 0.45))
     END,
     GridHeight = CASE
-        WHEN ROUND(GridHeight * 0.4) % 2 = 1
-            THEN MAX(10, ROUND(GridHeight * 0.4) - 1)
-        ELSE MAX(10, ROUND(GridHeight * 0.4))
+        WHEN ROUND(GridHeight * 0.45) % 2 = 1
+            THEN MAX(10, ROUND(GridHeight * 0.45) - 1)
+        ELSE MAX(10, ROUND(GridHeight * 0.45))
     END;
 
 -- Scale down natural wonders too. Technically this should be
